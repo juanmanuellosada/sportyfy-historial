051af74c215d9f11a7c71786ba4c920c41a554b8
Juanma Losada <juanmalosada01@gmail.com>
2023-10-11 21:17:21 -0300

    Primer commit

---
+++ src/main/java/sportyfy/historial/Main.java
@@ -0,0 +1,9 @@
+package sportyfy.historial;
+
+public class Main {
+    /* Hay que hacer una clase aparte, este proyecto no tendría Main. Falta agregar el core como dependencia con
+    * Jitpack en el build.gradle */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
